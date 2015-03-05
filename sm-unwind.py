420bef7814db03b1901f8f0e9963ce66e1d44cc6
Tom Tromey <tom@tromey.com>
2015-03-05 15:02:19 -0700

    rearrangements; start of arch and trampoline work

--- sm-unwind.py
+++ sm-unwind.py
@@ -2,9 +2,16 @@ import gdb
 import GdbJitReader
 import struct
 
-# Ouch.
-SP_REGNO = 7
-PC_REGNO = 16
+def get_pack_fmt(buffer)
+    if len(buffer) == 4:
+        fmt = 'I'
+    else:
+        # len == 8
+        fmt = 'L'
+    return fmt
+
+def unpack_addr(reg_buffer):
+    return struct.unpack_from(get_pack_fmt(reg_buffer), reg_buffer)
 
 FRAMETYPE_MASK = (1 << gdb.parse_and_eval('js::jit::FRAMETYPE_BITS')) - 1
 FRAMESIZE_SHIFT = gdb.parse_and_eval('js::jit::FRAMESIZE_SHIFT')
@@ -54,21 +61,18 @@ def callee_token_to_script(token):
     # FIXME
     # return Value(token).cast(gdb.lookup_type('js::JSScript').pointer())
 
-def get_pack_fmt(buffer)
-    if len(buffer) == 4:
-        fmt = 'I'
-    else:
-        # len == 8
-        fmt = 'L'
-    return fmt
-
-def unpack_addr(reg_buffer):
-    return struct.unpack_from(get_pack_fmt(reg_buffer), reg_buffer)
-
-def is_trampoline(addr):
-    # FIXME - we need special handling for trampoline frames
-    # this is just JitRuntime:: enterJIT_ and enterBaselineJIT_
-    return False
+def unwind_ordinary(pc, callbacks):
+    regs = []
+    sp = callbacks.get_register(SP_REGNO)
+    fmt = get_pack_fmt(sp)
+    sp = struct.unpack_from(fmt, sp)
+    descriptor = struct.unpack_from(fmt, callbacks.read_memory(sp, size))
+    regs[PC_REGNO] = callbacks.read_memory(sp + size, size)
+    args_size = descriptor >> FRAMESIZE_SHIFT
+    frame_type = descriptor & FRAMETYPE_MASK
+    type_size = type_sizes[frame_type]
+    regs[SP_REGNO] = struct.pack(fmt, sp + args_size + type_size)
+    return regs
 
 class SpiderMonkeyUnwinder(object):
     def unwind(self, callbacks):
@@ -77,18 +81,11 @@ class SpiderMonkeyUnwinder(object):
         pc = unpack_addr(callbacks.get_register(PC_REGNO))
         if gdb.solib_name(pc) is not None:
             return False
-        # FIXME detecting a trampoline frame.
-        regs = []
-        sp = callbacks.get_register(SP_REGNO)
-        fmt = get_pack_fmt(sp)
-        sp = struct.unpack_from(fmt, sp)
-        descriptor = struct.unpack_from(fmt, callbacks.read_memory(sp, size))
-        regs[PC_REGNO] = callbacks.read_memory(sp + size, size)
-        args_size = descriptor >> FRAMESIZE_SHIFT
-        frame_type = descriptor & FRAMETYPE_MASK
-        type_size = type_sizes[frame_type]
-        regs[SP_REGNO] = struct.pack(fmt, sp + args_size + type_size)
-        return regs
+
+        if self.is_trampoline(pc):
+            return self.unwind_trampoline(pc, callbacks)
+
+        return unwind_ordinary(pc, callbacks)
 
     def get_frame_id(self, callbacks):
         sp = callbacks.get_register(SP_REGNO)
@@ -99,4 +96,29 @@ class SpiderMonkeyUnwinder(object):
         pc = struct.unpack_from(fmt, callbacks.read_memory(sp + size, size))
         return (pc, sp)
 
-GdbJitReader.register_jit_reader(SpiderMonkeyUnwinder())
+class x64_info(SpiderMonkeyUnwinder):
+    SP_REGNO = 7
+    PC_REGNO = 16
+
+    # FIXME define other registers here.
+
+    def is_trampoline(self, pc):
+        # FIXME - we need special handling for trampoline frames
+        # this is just JitRuntime:: enterJIT_ and enterBaselineJIT_
+        # Maybe this can move to the base class
+        return False
+
+    def unwind_trampoline(self, pc, callbacks):
+        sp = callbacks.get_register(SP_REGNO)
+        wordsize = len(sp)
+        sp = unpack_addr(sp)
+        regs = []
+        # Must be in sync with Trampoline-x64.cpp:generateEnterJIT.
+        pushed_regs = [rbp, rbx, r12, r13, r14, r15]
+        for reg in pushed_regs:
+            sp = sp - wordsize
+            regs[reg] = callbacks.read_memory(sp, wordsize)
+        return regs
+
+# FIXME
+GdbJitReader.register_jit_reader(x64_info())

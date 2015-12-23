2af9f6d0b3a7536c0eca0f98172b3d79d9da30cd
Tom Tromey <tom@tromey.com>
2015-12-23 11:49:21 -0700

    add some comments

--- sm-unwind.py
+++ sm-unwind.py
@@ -7,7 +7,8 @@ try:
     from gdb.unwinder import Unwinder
 except ImportError:
     _have_unwinder = False
-    # We need something here.
+    # We need something here; it doesn't matter what as no unwinder
+    # will ever be instantiated.
     Unwinder = object
 
 def debug(something):
@@ -42,6 +43,8 @@ SizeOfFramePrefix = {
     'JitFrame_Unwound_IonAccessorIC': 'IonAccessorICFrameLayout',
 }
 
+# All types and symbols that we need are attached to an object that we
+# can dispose of as needed.
 class UnwinderTypeCache(object):
     def __init__(self):
         self.FRAMETYPE_MASK = (1 << gdb.parse_and_eval('js::jit::FRAMETYPE_BITS')) - 1
@@ -92,6 +95,9 @@ def parse_proc_maps():
             mappings.append((int(start, 16), int(end, 16)))
     return mappings
 
+# This represents a single JIT frame for the purposes of display.
+# That is, the frame filter creates instances of this when it sees a
+# JIT frame in the stack.
 class JitFrameDecorator(FrameDecorator):
     def __init__(self, base, info):
         super(JitFrameDecorator, self).__init__(base)
@@ -102,7 +108,12 @@ class JitFrameDecorator(FrameDecorator):
             return "<<" + self.info["name"] + ">>"
         return FrameDecorator.function(self)
 
+# A frame filter for SpiderMonkey.
 class SpiderMonkeyFrameFilter(object):
+    # |state_holder| is either None, or an instance of
+    # SpiderMonkeyUnwinder.  If the latter, then this class will
+    # reference the |unwinder_state| attribute to find the current
+    # unwinder state.
     def __init__(self, state_holder):
         self.name = "SpiderMonkey"
         self.enabled = True
@@ -121,57 +132,91 @@ class SpiderMonkeyFrameFilter(object):
     def filter(self, frame_iter):
         return map(self.maybe_wrap_frame, frame_iter)
 
+# A frame id class, as specified by the gdb unwinder API.
 class SpiderMonkeyFrameId(object):
     def __init__(self, sp, pc):
         self.sp = sp
         self.pc = pc
 
+# This holds all the state needed during a given unwind.  Each time a
+# new unwind is done, a new instance of this class is created.  It
+# keeps track of all the state needed to unwind JIT frames.  Note that
+# this class is not directly instantiated.
+#
+# This is a base class, and must be specialized for each target
+# architecture, both because we need to use arch-specific register
+# names, and because entry frame unwinding is arch-specific.
+# See https://sourceware.org/bugzilla/show_bug.cgi?id=19286 for info
+# about the register name issue.
+#
+# Each subclass must define SP_REGISTER and PC_REGISTER and implement
+# unwind_entry_frame.
 class UnwinderState(object):
-    # We have to use the arch-specific register names.
-    # See https://sourceware.org/bugzilla/show_bug.cgi?id=19286
-    # So, each subclass must define SP_REGISTER and PC_REGISTER and
-    # implement unwind_entry_frame.
-
     def __init__(self, typecache):
         debug("@@ new UnwinderState")
         self.next_sp = None
         self.next_type = None
         self.activation = None
+        # An unwinder instance is specific to a thread.  Record the
+        # selected thread for later verification.
         self.thread = gdb.selected_thread()
         self.frame_map = {}
         self.proc_mappings = parse_proc_maps()
         self.typecache = typecache
 
+    # If the given gdb.Frame was created by this unwinder, return the
+    # corresponding informational dictionary for the frame.
+    # Otherwise, return None.  This is used by the frame filter to
+    # display extra information about the frame.
     def get_frame(self, frame):
         sp = int(frame.read_register(self.SP_REGISTER))
         if sp in self.frame_map:
             return self.frame_map[sp]
         return None
 
+    # Add information about a frame to the frame map.  This map is
+    # queried by |self.get_frame|.  |sp| is the frame's stack pointer,
+    # and |dictionary| holds any extra information about the frame.
+    # Currently the only defined member of the dictionary is "name",
+    # which holds the frame's type as a string, e.g. "JitFrame_Exit".
     def add_frame(self, sp, dictionary):
         self.frame_map[int(sp)] = dictionary
 
+    # See whether |pc| is claimed by some text mapping.  See
+    # |parse_proc_maps| for details on how the decision is made.
     def text_address_claimed(self, pc):
         for (start, end) in self.proc_mappings:
             if (pc >= start and pc <= end):
                 return True
         return False
 
+    # Check whether |self| is valid for the selected thread.
     def check(self):
         return gdb.selected_thread() is self.thread
 
+    # Essentially js::TlsPerThreadData.get().
     def get_tls_per_thread_data(self):
         return self.typecache.per_tls_data.value()['mValue']
 
+    # |common| is a pointer to a CommonFrameLayout object.  Return a
+    # tuple (size, frame_type), where |size| is the integer size of
+    # the frame in bytes, and |frame_type| is an integer representing
+    # the frame type.
     def unpack_descriptor(self, common):
         value = common['descriptor_']
         size = int(value >> self.typecache.FRAMESIZE_SHIFT)
         frame_type = int(value & self.typecache.FRAMETYPE_MASK)
         return (size, frame_type)
 
+    # Given a frame_type, return the base size of the frame in bytes.
     def sizeof_frame_type(self, frame_type):
         return self.typecache.frame_size_map[frame_type]
 
+    # Create a new frame for gdb.  |sp| is the stack pointer to use,
+    # and |pending_frame| is the pending frame (see the gdb unwinder
+    # documentation).  This uses the pending frame to make unwind
+    # information for gdb.  This unwind info is returned.  This also
+    # registers the newly-created frame using |self.add_frame|.
     def create_frame(self, sp, pending_frame):
         common = sp.cast(self.typecache.typeCommonFrameLayoutPointer)
         debug("@@ common = 0x%x : %s" % (int(sp), str(common.dereference())))
@@ -188,8 +233,12 @@ class UnwinderState(object):
             frame_size = self.sizeof_frame_type(frame_type)
         self.next_sp = sp + size + frame_size
         frame_id = SpiderMonkeyFrameId(sp, new_pc)
-        # Register this frame so the frame filter can find it.
-        self.add_frame(sp, { "name": self.typecache.frame_enum_names[self.next_type] })
+        # Register this frame so the frame filter can find it.  This
+        # would be a good spot to try to fetch the function object,
+        # arguments, etc.
+        self.add_frame(sp, {
+            "name": self.typecache.frame_enum_names[self.next_type]
+        })
         # FIXME it would be great to unwind any other registers here.
         unwind_info = pending_frame.create_unwind_info(frame_id)
         # gdb mysteriously doesn't do this automatically.
@@ -198,10 +247,15 @@ class UnwinderState(object):
         unwind_info.add_saved_register(self.SP_REGISTER, frame_id.sp)
         return unwind_info
 
+    # Unwind an "ordinary" JIT frame.  This is used for JIT frames
+    # other than enter and exit frames.  Returns the newly-created
+    # unwind info for gdb.
     def unwind_ordinary(self, pending_frame):
         debug("@@ unwind_ordinary")
         return self.create_frame(self.next_sp, pending_frame)
 
+    # Unwind an exit frame.  Returns None if this cannot be done;
+    # otherwise returns the newly-created unwind info for gdb.
     def unwind_exit_frame(self, pending_frame):
         if self.activation == 0:
             debug("@@ unwind_exit_frame: end")
@@ -226,6 +280,9 @@ class UnwinderState(object):
         self.next_type = self.typecache.frame_enum_values['JitFrame_Exit']
         return self.create_frame(jittop, pending_frame)
 
+    # The main entry point that is called to try to unwind a JIT frame
+    # of any type.  Returns None if this cannot be done; otherwise
+    # returns the newly-created unwind info for gdb.
     def unwind(self, pending_frame):
         pc = pending_frame.read_register(self.PC_REGISTER)
 
@@ -247,9 +304,8 @@ class UnwinderState(object):
         # the time being.
         return self.unwind_exit_frame(pending_frame)
 
+# The UnwinderState subclass for x86-64.
 class x64UnwinderState(UnwinderState):
-    # FIXME this means we need per-arch subclasses of UnwinderState.
-    # (But we need that anyway for trampoline frames)
     SP_REGISTER = 'rsp'
     PC_REGISTER = 'rip'
     # Must be in sync with Trampoline-x64.cpp:generateEnterJIT.  Note
@@ -257,14 +313,16 @@ class x64UnwinderState(UnwinderState):
     # previous function's call.
     PUSHED_REGS = ["r15", "r14", "r13", "r12", "rbx", "rbp", "rip"]
 
+    # Unwind an entry frame.  Returns the newly-created unwind info
+    # for gdb.
     def unwind_entry_frame(self, sp, pending_frame):
         debug("@@ unwind_entry_frame")
         debug("@@ entry sp = 0x%x" % int(sp))
         void_starstar = gdb.lookup_type('void').pointer().pointer()
         sp = sp.cast(void_starstar)
         # We have to unwind the registers first, then create the frame
-        # id.  So we have to stash the registers in a temporary
-        # dictionary here.
+        # id.  So we stash the registers in a temporary dictionary
+        # here.
         regs = {}
         # Skip the "result" push.
         sp = sp + 1
@@ -283,6 +341,9 @@ class x64UnwinderState(UnwinderState):
             unwind_info.add_saved_register(reg, regs[reg])
         return unwind_info
 
+# The unwinder object.  This provides the "user interface" to the JIT
+# unwinder, and also handles constructing or destroying UnwinderState
+# objects as needed.
 class SpiderMonkeyUnwinder(Unwinder):
     def __init__(self, typecache):
         super(SpiderMonkeyUnwinder, self).__init__("SpiderMonkey")
@@ -301,6 +362,8 @@ class SpiderMonkeyUnwinder(Unwinder):
     def invalidate_unwinder_state(self, *args, **kwargs):
         self.unwinder_state = None
 
+# Register the unwinder and frame filter with |objfile|.  If |objfile|
+# is None, register them globally.
 def register_unwinder(objfile):
     unwinder = None
     if _have_unwinder:
